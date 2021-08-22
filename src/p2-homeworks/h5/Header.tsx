8df81c82b68a94e2fab5d4d950d5eea5f965cea1
iForced <iforce@2ch.hk>
2021-08-22 23:39:06 +0300

    add HW5

--- src/p2-homeworks/h5/Header.tsx
+++ src/p2-homeworks/h5/Header.tsx
@@ -1,10 +1,14 @@
 import React from 'react'
+import './HW5.css'
+import { NavLink } from 'react-router-dom'
 
 function Header() {
     return (
-        <div>
-            // add NavLinks
-
+        //TODO add animation for navbar
+        <div className={"navbar"}>
+            <NavLink className={"nav_link"} to={'/pre-junior'} activeClassName={"active_link"}>Pre Junior</NavLink>
+            <NavLink className={"nav_link"} to={'/junior'} activeClassName={"active_link"}>Junior</NavLink>
+            <NavLink className={"nav_link"} to={'/junior-plus'} activeClassName={"active_link"}>Junior Plus</NavLink>
         </div>
     )
 }
---
+++ src/p2-homeworks/h5/pages/JuniorPlus.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function JuniorPlus() {
+    return (
+        <div>
+            Junior Plus page
+        </div>
+    )
+}
+
+export default JuniorPlus
