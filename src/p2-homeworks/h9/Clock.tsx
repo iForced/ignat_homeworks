c2dac2cb540e6f64454e91f82ef0050acf6340ae
iForced <iforce@2ch.hk>
2021-09-20 21:52:02 +0300

    make HW9

--- src/p2-homeworks/h9/Clock.tsx
+++ src/p2-homeworks/h9/Clock.tsx
@@ -1,35 +1,39 @@
 import React, {useState} from 'react'
 import SuperButton from '../h4/common/c2-SuperButton/SuperButton'
+import moment from "moment/moment";
+import {Moment} from "moment";
 
 function Clock() {
     const [timerId, setTimerId] = useState<number>(0)
-    const [date, setDate] = useState<Date>()
+    const [date, setDate] = useState<Moment>(moment())
     const [show, setShow] = useState<boolean>(false)
 
     const stop = () => {
-        // stop
+        clearInterval(timerId)
     }
     const start = () => {
         stop()
         const id: number = window.setInterval(() => {
-            // setDate
+            const newDate = moment()
+            setDate(newDate)
         }, 1000)
         setTimerId(id)
     }
 
     const onMouseEnter = () => {
-        // show
+        setShow(true)
     }
     const onMouseLeave = () => {
-        // close
+        setShow(false)
     }
 
-    const stringTime = 'Time' // fix with date
-    const stringDate = 'Date' // fix with date
+    const stringTime = date.format('HH mm ss')
+    const stringDate = date.format('DD MMM YYYY')
 
     return (
         <div>
             <div
+                style={{cursor: "help"}}
                 onMouseEnter={onMouseEnter}
                 onMouseLeave={onMouseLeave}
             >
